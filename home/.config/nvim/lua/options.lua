24fe250dd7c86ef7e5454dc7b39b1aef0d910c81
Ryoh Akiyoshi <ryo614@gmail.com>
2022-04-02 21:59:44 +0900

    Refactor

--- home/.config/nvim/lua/options.lua
+++ home/.config/nvim/lua/options.lua
@@ -1,40 +1,39 @@
-vim.o.smarttab = true
-vim.o.expandtab = true
-vim.o.tabstop = 4
-vim.o.smartindent = true
-vim.o.shiftwidth = 4
+local opt = vim.opt
+local opt_global = vim.go
+
+
+--------------------------
+-- Local and Global (set)
+--------------------------
+
 -- Show number
-vim.o.number = true
+opt.number = true
 -- Encodings
-vim.o.encoding = 'utf-8'
-vim.o.fileencodings = 'utf-8,iso-2022-jp,euc-jp,sjis'
+opt.encoding = 'utf-8'
+opt.fileencodings = 'utf-8,iso-2022-jp,euc-jp,sjis'
 -- Split directions
-vim.o.splitright = true
-vim.o.splitbelow = true
+opt.splitright = true
+opt.splitbelow = true
 -- Persistent undo
-vim.o.undodir = vim.env.HOME .. '/.config/nvim/undo'
-vim.o.undofile = true
+opt.undodir = vim.env.HOME .. '/.config/nvim/undo'
+opt.undofile = true
 -- Clipboard (mac)
 if jit.os == "Darwin" then
-  vim.o.clipboard = 'unnamed'
+  opt.clipboard = 'unnamed'
 end
 -- Do not hide quote of JSON
-vim.o.conceallevel = 0
+opt.conceallevel = 0
 -- auto reload
-vim.o.autoread = true
+opt.autoread = true
 
--- Indent Settings
-vim.cmd('augroup filetypedetect')
-vim.cmd('autocmd!')
-vim.cmd('autocmd FileType yaml setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType sh setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType ruby setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType zsh setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType tf setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType vim setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType typescript setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType typescriptreact setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType json setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType markdown setlocal ts=2 sts=2 sw=2')
-vim.cmd('autocmd FileType go setlocal noexpandtab')
-vim.cmd('augroup END')
+
+----------------------
+-- Global (setglobal)
+----------------------
+
+-- Indent
+opt_global.smarttab = true
+opt_global.expandtab = true
+opt_global.tabstop = 4
+opt_global.smartindent = true
+opt_global.shiftwidth = 4
